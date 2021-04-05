dcaf7e0a481a451e56cc42cdc51211895f72ef26
noordseoostende <voropayev1@mail.ru>
2021-04-05 21:40:09 +0200

    een fout opgetreden was in courses.js, ik heb deze opgelost

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,9 +8,70 @@ const Bootcamp = require('../models/Bootcamp');
 // @access  Public
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+  
+  // Cope req.query
+  const reqQuery = { ...req.query };
 
-    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+  // Fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+  
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // Finding resource
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
+
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  // Executing query
+  const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
+
+    res.status(200).json({ success: true, count: bootcamps.length, pagination, data: bootcamps });
 });
 
 // @desc    Get single bootcamp
@@ -74,19 +135,20 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
   
     if (!bootcamp) {
       return next(
         new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
+    bootcamp.remove();
   
     res.status(200).json({ success: true, data: {} });
   
   
 });
 
-// @desc    Gae bootcamps within a radius
+// @desc    Get bootcamps within a radius
 // @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
 // @access  Private 
 
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,7 +8,14 @@ const {
   deleteBootcamp,
   getBootcampsInRadius
 } = require('../controllers/bootcamps');
+
+// Include other resource router
+const courseRouter = require('./courses');
+
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
