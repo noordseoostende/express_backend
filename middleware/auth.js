f301bbf4baea1df0ba7726a868a75f6c22e7c3b3
noordseoostende <voropayev1@mail.ru>
2021-04-10 21:22:27 +0200

    de gebruikers kunnen hun wachtwoord veranderen

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,9 @@ const User = require('../models/User');
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')
+  if(
+    req.headers.authorization && 
+    req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
@@ -18,7 +20,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
   // Make sure token exists
   if(!token) {
-    return next(new ErrorResponse('Not authorize t access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
   try {
     // Verify token
@@ -30,6 +32,16 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize t access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if(!roles.includes(req.user.role)) {
+      return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access this route`, 403));
+    }
+    next();
+  }
+}
